19fe6b82073a89973fc8fdd561edca1a185a4556
saurabhkap398 <83824249+saurabhkap398@users.noreply.github.com>
2021-05-09 23:53:56 +0530

    Add files via upload

---
+++ assigment2/PP9.java
@@ -0,0 +1,29 @@
+class PP9
+{
+  public static void main(String args[])
+  { 
+   for(int i=1;i<=5;i++)
+		{ 
+			for(int j=4;j>=i;j--)
+			{ 
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("*");
+			}System.out.println();
+		}
+	for( int i=1; i<=5;i++)
+        {
+			for( int j=1; j<=i;j++)
+			{
+			System.out.print(" ");
+			}
+			for (int j=i;j<=4;j++)
+			{
+			System.out.print("*");
+			
+			}System.out.println();
+		}  
+	  
+}}
