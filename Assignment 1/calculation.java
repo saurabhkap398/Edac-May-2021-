5b4a199696bcf95ddbf7563daa0ce017c8e3750c
saurabhkap398 <83824249+saurabhkap398@users.noreply.github.com>
2021-05-10 00:04:26 +0530

    Add files via upload

---
+++ Assignment 1/calculation.java
@@ -0,0 +1,32 @@
+//Que 4
+
+ public class calculation
+  {
+   public static void main (String args[])
+   {
+	int a,b,c,d,e,f,g,h,i,ans;
+	a= 5;
+	b= 8;
+	c= 6;
+	d= 55;
+	e= 9;
+	f= 20;
+	g= 3;
+	h= 15;
+	i= 2;
+	
+	ans = -a+b*c;
+	System.out.println("-5+8*6 = "+ans);
+	
+	ans = (d+e)%e;
+	System.out.println(+ans);
+	
+	ans = 20+(-3*5)/8;
+	System.out.println(+ans);
+	
+	
+	System.out.println(+(5+(15/3)*2-(8%3)));
+	
+   }
+   
+   }
---
+++ Assignment 1/sum.java
@@ -0,0 +1,14 @@
+//Que 2
+
+ public class sum
+  {
+   public static void main (String args[])
+   {
+	int i = 74;
+	int j = 36;
+	int k = i+j;
+   System.out.println("Sum : " +k);
+  
+   }
+   
+   }
