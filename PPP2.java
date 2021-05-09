688539a8798733a070892414a51dcc85e9d53ebd
saurabhkap398 <83824249+saurabhkap398@users.noreply.github.com>
2021-05-09 23:42:31 +0530

    Add files via upload

---
+++ PPP2.java
@@ -0,0 +1,16 @@
+class PPP2
+{ 
+  public static void main(String args[])
+  {
+     for(int i=1;i<=9;i++)
+      { 
+        for(int j=8;j>=i;j--)
+	     {
+	      System.out.print(" ");
+	     }
+	    for(int k=1;k<=i;k++)
+	     {
+	      System.out.print(k+" ");
+	     }System.out.println();
+	}
+}}
